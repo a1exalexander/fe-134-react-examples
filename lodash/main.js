f878e245dbf43c3606dcde233c97f036c0b1abc7
Oleksandr Ratushnyi <oleksandr.ratushnyi@namecheap.com>
2024-10-14 20:06:39 +0300

    add netlify functions example

---
+++ lodash/main.js
@@ -0,0 +1,13 @@
+import { add,round } from "lodash";
+
+const value = [
+  { name: "Fred", age: 11 },
+  { name: "Anna", age: 22 },
+  { name: "Bob", age: 40 },
+  { name: "Bob", age: 23 },
+];
+
+// const names = value.length;
+// const names2 = map(value, "name");
+
+console.log(round(add(0.1, 0.2), 2));
--- router-essential/src/App/App.jsx
+++ router-essential/src/App/App.jsx
@@ -2,5 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "../router";
 
 export const App = () => {
+  console.log(import.meta.env.VITE_API_KEY);
+  
   return <RouterProvider router={router} />;
 };
